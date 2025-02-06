7b3d4858e78fa9ede632459ae52c714332d76bcb
unknown <133560614+PhucPhuc1111@users.noreply.github.com>
2025-02-06 12:23:53 +0700

    update Health Profile API

--- NutriDiet.Service/ModelDTOs/Response/HealthProfileResponse.cs
+++ NutriDiet.Service/ModelDTOs/Response/HealthProfileResponse.cs
@@ -4,32 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NutriDiet.Service.ModelDTOs.Request
+namespace NutriDiet.Service.ModelDTOs.Response
 {
-    public class UserHealthRequest
+    public class HealthProfileResponse
     {
         public string FullName { get; set; } = null!;
         public string? Avatar { get; set; }
-
         public int? Age { get; set; }
-
         public string? Gender { get; set; }
-
         public string? Location { get; set; }
-
         public string? MedicalCondition { get; set; }
-
         public double? Height { get; set; }
-
         public double? Weight { get; set; }
-
         public string? ActivityLevel { get; set; }
-
         public string? HealthGoal { get; set; }
-
         public double? TargetWeight { get; set; }
-
         public DateTime? DurationTarget { get; set; }
-
     }
+
 }
--- NutriDiet.Service/Services/HealthProfileService.cs
+++ NutriDiet.Service/Services/HealthProfileService.cs
@@ -1,10 +1,13 @@
 ﻿using Mapster;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using NutriDiet.Common;
 using NutriDiet.Common.BusinessResult;
 using NutriDiet.Repository.Interface;
 using NutriDiet.Repository.Models;
 using NutriDiet.Service.Interface;
 using NutriDiet.Service.ModelDTOs.Request;
+using NutriDiet.Service.ModelDTOs.Response;
 using System;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -53,6 +56,64 @@ namespace NutriDiet.Service.Services
                 healthProfile.UserId = existingUser.UserId; 
 
                 await _unitOfWork.HealthProfileRepository.AddAsync(healthProfile);
+                await _unitOfWork.SaveChangesAsync();
+
+                await _unitOfWork.CommitTransaction();
+            }
+            catch (Exception)
+            {
+                await _unitOfWork.RollbackTransaction();
+                throw;
+            }
+        }
+
+        public async Task<IBusinessResult> GetHealthProfile()
+        {
+            var userid = int.Parse(_userIdClaim);
+
+            var existingUser = await _unitOfWork.UserRepository.GetByIdAsync(userid);
+            if (existingUser == null)
+            {
+                throw new Exception("User not exist.");
+            }
+
+            var healthProfile = await _unitOfWork.HealthProfileRepository
+                                        .GetByWhere(hp => hp.UserId == userid)
+                                        .FirstOrDefaultAsync();
+
+            var response = new HealthProfileResponse();
+            existingUser.Adapt(response);
+            if (healthProfile != null)
+            {
+                healthProfile.Adapt(response);
+            }
+
+            return new BusinessResult(Const.HTTP_STATUS_OK, Const.SUCCESS_READ_MSG, response);
+        }
+
+        public async Task UpdateHealthProfile(UserHealthRequest request)
+        {
+            var userid = int.Parse(_userIdClaim);
+
+            var existingUser = await _unitOfWork.UserRepository.GetByIdAsync(userid);
+            if (existingUser == null)
+            {
+                throw new Exception("User not exist.");
+            }
+
+            request.Adapt(existingUser);
+
+            var healthProfile = request.Adapt<HealthProfile>();
+
+            await _unitOfWork.BeginTransaction();
+            try
+            {
+                await _unitOfWork.UserRepository.UpdateAsync(existingUser);
+                await _unitOfWork.SaveChangesAsync();
+
+                healthProfile.UserId = existingUser.UserId;
+
+                await _unitOfWork.HealthProfileRepository.UpdateAsync(healthProfile);
                 await _unitOfWork.SaveChangesAsync();
 
                 await _unitOfWork.CommitTransaction();
