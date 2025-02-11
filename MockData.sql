9136c7f344abfb8afa2be668ef09d1d93de4c153
tungske <trinhsontung2410@gmail.com>
2025-02-11 16:31:15 +0700

    Update MockData.sql

--- MockData.sql
+++ MockData.sql
@@ -5,3 +5,10 @@ GO
 INSERT INTO Role (RoleID, RoleName) VALUES
 (1, 'Admin'),
 (2, 'Customer');
+
+SET IDENTITY_INSERT [dbo].[User] ON 
+GO
+INSERT [dbo].[User] ([UserID], [FullName], [Email], [Password], [Phone], [Age], [Gender], [Location], [Avatar], [fcmToken], [Status], [RoleID]) VALUES (1, N'New User', N'user@example.com', N'AQAAAAIAAYagAAAAEJC6r2LMSMDB1nSfXeYadVFihZL+PHOrpKK4g6s0kDy9LRR4sYRlbYbjDh3pF95RZg==', NULL, NULL, NULL, NULL, N'', NULL, N'Active', 2)
+GO
+SET IDENTITY_INSERT [dbo].[User] OFF
+GO
