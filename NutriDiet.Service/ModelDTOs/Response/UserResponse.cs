a9a204cbe7dbd49f53cacaab2448773552810961
Minh Mạnh <tivip1216vn@gmail.com>
2025-02-24 10:30:37 +0700

    fix api user

--- NutriDiet.Service/ModelDTOs/Response/UserResponse.cs
+++ NutriDiet.Service/ModelDTOs/Response/UserResponse.cs
@@ -8,6 +8,7 @@ namespace NutriDiet.Service.ModelDTOs.Response
 {
     public class UserResponse
     {
+        public int UserId { get; set; }
         public string FullName { get; set; } = null!;
 
         public string Email { get; set; } = null!;
