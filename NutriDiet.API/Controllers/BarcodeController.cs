ffe13dbd6060b95f0cff6068d2c02a5beacb9261
tungske <trinhsontung2410@gmail.com>
2025-04-05 14:28:02 +0700

    Update BarcodeController.cs

--- NutriDiet.API/Controllers/BarcodeController.cs
+++ NutriDiet.API/Controllers/BarcodeController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NutriDiet.Service.Utilities;
 
 namespace NutriDiet.API.Controllers
 {
     [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
     public class BarcodeController : ControllerBase
     {
