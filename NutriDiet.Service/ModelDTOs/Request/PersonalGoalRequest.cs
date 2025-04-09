c052db2294109017b36d22462669b452673c512b
Minh Mạnh <tivip1216vn@gmail.com>
2025-04-09 11:45:41 +0700

    fix validate

--- NutriDiet.Service/ModelDTOs/Request/PersonalGoalRequest.cs
+++ NutriDiet.Service/ModelDTOs/Request/PersonalGoalRequest.cs
@@ -1,6 +1,7 @@
 ﻿using NutriDiet.Common.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace NutriDiet.Service.ModelDTOs.Request
 {
     public class PersonalGoalRequest
     {
-        public GoalType GoalType { get; set; } 
+        public GoalType GoalType { get; set; }
+        [Range(30, 250,
+            ErrorMessage = "TargetWeight must be between 30 and 250 kg")]
         public double? TargetWeight { get; set; }
         public WeightChangeRate? WeightChangeRate { get; set; }
         public string GoalDescription { get; set; } = null!;
